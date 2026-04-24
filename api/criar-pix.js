7e32ffea5d6f346d9a696b4f663719f7ab4cf7c3
savinostoreacont-eng <savinostoreacont@gmail.com>
2026-04-24 19:14:20 -0300

    Create criar-pix.js

---
+++ api/criar-pix.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  const response = await fetch("https://api.misticpay.com/api/transactions/create", {
+    method: "POST",
+    headers: {
+      "ci": process.env.CI,
+      "cs": process.env.CS,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      amount: 25,
+      payerName: "Cliente",
+      payerDocument: "12345678909",
+      transactionId: "tx-" + Date.now(),
+      description: "Pagamento IPTV"
+    })
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
